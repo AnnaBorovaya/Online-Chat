fc6745db23f2c5b1e14bd49fb375f1fbb2f9ab03
Your NameAnnaBorovaya <anna.kviten11@gmail.com>
2018-11-16 13:28:46 +0200

    'chat'

---
+++ js/js.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function toggleClass(clickTarget,classTarget,className){
+    var clickTargetVariable = document.querySelector(clickTarget),
+        classTargetVariable = document.querySelector(classTarget);
+
+        function clickListener(){
+            event.stopPropagation();
+            this.classList.toggle('toggle-active');
+            classTargetVariable.classList.toggle(className);
+        }
+
+        clickTargetVariable.addEventListener('click',clickListener);
+
+        document.addEventListener('click',function(e){
+            if(e.target.closest(classTarget)){
+                return;
+            }
+            if(classTargetVariable.classList.contains(className)){
+                classTargetVariable.classList.remove(className);
+                clickTargetVariable.classList.remove('toggle-active');
+            }
+        })
+    
+}
+
+
+
+toggleClass('.togle-button','.side-bar','side-bar-open');
+toggleClass('.account-status','.acc-menu','acc-menu-open');
+toggleClass('.toggle-box','body','night-mode');
